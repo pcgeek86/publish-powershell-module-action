71852d564b5d33e6882347ee4a8ff06b34fa3ca5
Trevor Sullivan <trevor@balena.io>
2020-09-28 18:14:27 -0600

    Update entrypoint script

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,6 +1,8 @@
 #!/usr/bin/env pwsh
 
 Write-Host $env:INPUT_MODULEPATH
-Get-ChildItem -Path env:
-Write-Host hi
-#Publish-Module -Path $env:MODULE_PATH -NuGetApiKey $env:NUGET_API_KEY
+Write-Host $env:INPUT_NUGETAPIKEY
+
+Write-Host -Object ('Publishing module ({0}) to PowerShell Gallery' -f $env:INPUT_MODULEPATH)
+Publish-Module -Path $env:INPUT_MODULEPATH -NuGetApiKey $env:INPUT_NUGETAPIKEY
+Write-Host -Object 'Finished publishing module to PowerShell Gallery'
