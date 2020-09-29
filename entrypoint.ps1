dddec3d3ea162c50d471de91ee16dbb48efd6311
Trevor Sullivan <trevor@balena.io>
2020-09-28 19:02:57 -0600

    Update GitHub action

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -5,5 +5,6 @@ Write-Host $env:INPUT_NUGETAPIKEY
 
 Get-ChildItem -Path env:
 Write-Host -Object ('Publishing module ({0}) to PowerShell Gallery' -f $env:INPUT_MODULEPATH)
-Publish-Module -Path $env:RUNNER_WORKSPACE/$env:INPUT_MODULEPATH -NuGetApiKey $env:INPUT_NUGETAPIKEY
+Import-Module -Name $env:GITHUB_WORKSPACE/$env:INPUT_MODULEPATH
+Publish-Module -Name $env:INPUT_MODULENAME -NuGetApiKey $env:INPUT_NUGETAPIKEY
 Write-Host -Object 'Finished publishing module to PowerShell Gallery'
