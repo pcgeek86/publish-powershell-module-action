d19452d8f0442b6a77a5b75f52cdb0c99d42f99c
Chris Peterson <chris.peterson@gettyimages.com>
2021-03-31 14:10:26 -0700

    Prune output
    Most of this is debug level.  Printing out env variables can be a security vulnerability

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,13 +1,6 @@
 #!/usr/bin/env pwsh
 
-Write-Host $env:INPUT_MODULEPATH
-Write-Host $env:INPUT_NUGETAPIKEY
-
-Get-ChildItem -Path env:
-Write-Host -Object ('Publishing module ({0}) to PowerShell Gallery' -f $env:INPUT_MODULEPATH)
-
-Get-ChildItem -Path $env:GITHUB_WORKSPACE | Format-Table -AutoSize
-Get-Module | Format-Table -AutoSize
+Write-Host "Publishing module ($env:INPUT_MODULEPATH) to PowerShell Gallery"
 
 Publish-Module -Path $env:INPUT_MODULEPATH -NuGetApiKey $env:INPUT_NUGETAPIKEY
-Write-Host -Object 'Finished publishing module to PowerShell Gallery'
+Write-Host 'Finished publishing module to PowerShell Gallery'
